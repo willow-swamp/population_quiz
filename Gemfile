409cf00d95d26f503729928b253b3539ff0a2cfe
willow-swamp <yusuke.y19970103@outlook.com>
2024-04-29 23:59:20 +0900

    Add:i18nとmeta-tagsの導入

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 gem "cssbundling-rails"
+gem "rails-i18n"
+
+gem "meta-tags"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'PopuGuesser',
+      title: :title,
+      reverse: true,
+      description: '日本の都道府県や市町村の人口を当てるクイズです。',
+      keywords: '日本,人口,都道府県,市町村,クイズ',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        title: :title,
+        type: 'website',
+        url: request.original_url,
+        site_name: :site,
+        description: :description,
+      },
+      twitter: {
+        site: '@liverlog',
+      }
+    }
+  end
 end
